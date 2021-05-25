21efb2a5980f20daf79ad3dd2410611cb2ca0792
Brian OConnor <boconnor20@gmail.com>
2021-05-25 17:46:06 -0500

    functions to retrieve answers/questions and timers updated

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-// setting up variables
 var startButton = document.querySelector(".start-button ");
 var timerElemet = document.querySelector(".timer-count");
 
+var timerCount;
+var highScore;
+var correct;
+var wrong;
+var questions = document.querySelector("#questions");
+var choices = document.querySelector("#choices");
+var index = 0;
 
 
 
-
-
-function startGame() {
-
+function startQuiz() {
+  timerCount = 50;
 
 }
 
@@ -16,21 +20,24 @@ function startGame() {
 const quizQuestions = [
     {
        question: "How do call a funcion named myFunction?",
-       answers: {
-           a: "myfunction",
-           b: "myFunction()",
-           c: "Call myFunction()",
-       },
-       correctAnswer: "b"
+       answers: [
+            "myfunction",
+            "myFunction()",
+            "Call myFunction()",
+       ],
+       correct: "myFunction()",
+       
     },
     {
-        question: "What is the correct way to start a For loop?",
+        question: "What is the correct way to write a For loop?",
         answers: {
             a: "for (i <= 5; i++)",
             b: "For i =0, i => 10: i+",
             c: "for (i = 0; i <= 5; i++)",
         },
-        correctAnswer: "c"
+        correct: "c",
+        wrong: "a",
+        wrong: "b",
      },
      {
         question: "Which tag contains all of the website's visible content",
@@ -39,7 +46,9 @@ const quizQuestions = [
             b: "<head>",
             c: "<body>",
         },
-        correctAnswer: "c"
+        correct: "c",
+        wrong: "a",
+        wrong: "b",
      },
      {
         question: "How do you insert a comment in a CSS file",
@@ -48,7 +57,9 @@ const quizQuestions = [
             b: "// this is a comment //",
             c: "// this is a comment",
         },
-        correctAnswer: "a"
+        correct: "a",
+        wrong: "b",
+        wrong: "c",
      },
      {
         question: "Arrays are used to store what?",
@@ -57,7 +68,60 @@ const quizQuestions = [
             b: "JavaScript arrays are used to store multiple values in a single variable",
             c: "Arrays can be used to store containers from variables ",
         },
-        correctAnswer: "b"
+        correct: "b",
+        wrong: "c",
+        wrong: "a",
      },
     
 ];
+function getQuestions() {
+  var currentQuestion = quizQuestions [index];
+  questions.textContent = currentQuestion.question
+  choices.innerHTML = ""
+  currentQuestion.answers.forEach(function(choice,i){
+    var choiceBtn = document.createElement("button")
+    choiceBtn.setAttribute("class","choice")
+    choiceBtn.setAttribute("value",choice)
+    choiceBtn.textContent = i + 1 + "" + choice 
+    choices.appendChild(choiceBtn)
+    choiceBtn.onclick = verifyAnswer
+  })
+}
+
+function verifyAnswer() {
+  if (this.value !== quizQuestions [index].correct) {
+    timerCount -= 2
+  }
+  index ++
+  if (index === quizQuestions.length) {
+    quizOver () 
+
+  }
+  else {
+    getQuestions()
+  }
+}
+
+// Timer function/ additional decrement of time for wrong answers
+function startTimer() {
+  timer = setInterval(function() {
+  timerCount--;
+  timerElement.quizQuestions = timerCount;
+    if (timerCount >= 0) {
+     // NEED TO FIGURE THIS OUT
+    } else if (wrong && timerCount --- 2) {
+
+    } if (highScore && timerCount > 0) {
+          clearInterval(timer);
+    }
+      if (timerCount === 0) {
+        clearInterval(timer);
+    }
+  }, 50000);
+};
+function quizOver() {
+  clearInterval(timerCount)
+  questions.setAttribute("class","hide")
+  var endScreen = document.querySelector(".wrapper")
+  endScreen.removeAttribute("class")
+}
