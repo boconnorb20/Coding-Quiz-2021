ba6d3da099498fee161e5b33af3a9ade8547f608
Brian OConnor <boconnor20@gmail.com>
2021-05-31 21:12:17 -0500

    several js changes, linked highScore html page, working on calculating scores and getting 'play again' button functioning

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 var startButton = document.querySelector(".start-button");
 var timerElement = document.querySelector(".time");
-
-var timerCount;
+var timer;
+var timerCount=50;
 var highScore;
-var correct;
 var questions = document.querySelector("#questions");
-var choices = document.querySelector("#choices");
+var answers = document.querySelector("#answers");
 var index = 0;
-
- 
-timerCount = 50;
 
 
 var startTimer = function() {
-  // JS event "timer-event" doesn't exist
-  // timerElement.addEventListener("timer-event", startTimer);
+timerElement.addEventListener("timer-event", startTimer);
 
   // put timer count on the page visually
   timerElement.textContent = timerCount;
@@ -24,32 +19,48 @@ var startTimer = function() {
 
    // check on time left
    // check if questions remaining
-    if (timerCount >= 0) {
-     // NEED TO FIGURE THIS OUT
+    if (timerCount > 0) {
      // play next question
-    } else if (wrong && timerCount --- 2) {
+    } else {
 
-    } if (highScore && timerCount > 0) {
-        clearInterval(timer);
     }
-      if (timerCount === 0) {
-        clearInterval(timer);
-    }
+    if (quizOver);
+    clearInterval(timer);
+    
   }, 1000);
 };
 
+// starts game
 var startGame = function() {
+  // starts timer
   startTimer();
+  // play first question
   getQuestions();
   // doWhateverElseYouNeedAtStart();
 }
 
+// hook up the startGame function to the startButton button
 startButton.addEventListener('click', startGame);
 
+function getQuestions() {
+  var currentQuestion = quizQuestions[index];
+  questions.textContent = currentQuestion.question
+  answers.innerHTML = "answers"
+  currentQuestion.answers.forEach(function(choice, i){
+    var choiceBtn = document.createElement("button")
+    choiceBtn.setAttribute("class", "choice")
+    choiceBtn.setAttribute("value", choice)
+    choiceBtn.textContent = i + 1 + ": " + choice 
+    answers.appendChild(choiceBtn)
+    choiceBtn.onclick = verifyAnswer
+  })
+}
+
 // building questions and answers
-const quizQuestions = [
+const quizQuestions = [ 
     {
-       question: "How do call a funcion named myFunction?",
+
+       question: "How would you call a funcion named myFunction?",
        answers: [
             "myfunction",
             "myFunction()",
@@ -85,7 +96,7 @@ const quizQuestions = [
               "// this is a comment //",
               "// this is a comment",
         ],
-        correct: "this is a comment",
+        correct: "//this is a comment",
         
      },
      {
@@ -100,19 +111,7 @@ const quizQuestions = [
      },
     
 ];
-function getQuestions() {
-  var currentQuestion = quizQuestions[index];
-  questions.textContent = currentQuestion.question
-  choices.innerHTML = ""
-  currentQuestion.answers.forEach(function(choice, i){
-    var choiceBtn = document.createElement("button")
-    choiceBtn.setAttribute("class", "choice")
-    choiceBtn.setAttribute("value", choice)
-    choiceBtn.textContent = i + 1 + ": " + choice 
-    choices.appendChild(choiceBtn)
-    choiceBtn.onclick = verifyAnswer
-  })
-}
+
 
 // verify selected answer
 function verifyAnswer() {
@@ -136,17 +135,32 @@ function verifyAnswer() {
 }
 
 // ends quiz
+/**
+ * Needs to:
+ * - Stop Timer
+ * - Tally Score and update it in localStorage
+ * - Reset data like timer to 50 and index to 0
+ * - Start the High Score Flow (collect name, add score to list, show list)
+ * - window.location.href('highScoresPage.html');
+ */
+
 function quizOver() {
   // stops timer
   clearInterval(timerCount);
   // hides questions element
   questions.setAttribute("class", "hide");
   // to change page location
-  // window.location.href('highScoresPage.html');
 
-  var endScreen = document.querySelector(".wrapper")
+  window.location.href= ('highScoresPage.html');
+  var endScreen = document.querySelector('.wrapper')
   // remove class names from endScreen
   endScreen.removeAttribute("class")
 
   // reset index variable to 0
+}
+// creating scores 
+function scoreQuiz(answers, questions) {
+  var score = (answers/questions) * 100;
+  return score;
+
 }
